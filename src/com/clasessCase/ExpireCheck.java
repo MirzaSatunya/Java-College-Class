e684cf3cae51716c2b946e1be807de110d5a3127
Mirza Hilmi <miruza6748@gmail.com>
2022-10-13 13:54:36 +0700

    Created Expire Date Checker

---
+++ src/com/clasessCase/ExpireCheck.java
@@ -0,0 +1,14 @@
+package com.clasessCase;
+
+import java.util.Scanner;
+
+public class ExpireCheck {
+    int[] isEqual = { 0, 0 };
+    
+    Scanner sc = new Scanner(System.in);
+
+    String dateInput = sc.nextLine();
+    String expInput = sc.nextLine();
+
+    
+}
